fa3c92d579a82575aa66877222ce4678839be66b
Caio Hamamura <caiohamamura@gmail.com>
2018-06-23 12:16:39 -0300

    Parallel processing of proc1_08

---
+++ Organized/proc1_08_car_premium_3.sql
@@ -0,0 +1,24 @@
+SELECT setval('seq_current_run', (SELECT MAX(num_run) FROM lt_model.log_outputs));
+
+
+-- log premium
+INSERT INTO log_outputs (num_run, fk_operation, num_geom, val_area)
+	SELECT currval('seq_current_run'),
+	operation.id,
+	COUNT(*),
+	SUM(shape_area)
+FROM log_operation operation,
+proc1_03_is_premium b
+WHERE operation.nom_operation = 'car_premium' AND fla_car_premium
+GROUP BY operation.id;
+
+--log poor
+INSERT INTO log_outputs (num_run, fk_operation, num_geom, val_area)
+	SELECT currval('seq_current_run'),
+	operation.id,
+	COUNT(*),
+	SUM(shape_area)
+FROM log_operation operation,
+proc1_03_is_premium b
+WHERE operation.nom_operation = 'car_poor' AND NOT fla_car_premium
+GROUP BY operation.id;
