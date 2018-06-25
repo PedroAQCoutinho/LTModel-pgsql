a8ac393bb19522b7addd1f5edabe836687cdddcd
Caio Hamamura <caiohamamura@gmail.com>
2018-06-25 14:29:37 -0300

    Fixed proc1_14 CAR-SiGEF

--- Organized/proc1_14_car-sigef_1.sql
+++ Organized/proc1_14_car-sigef_1.sql
@@ -4,12 +4,10 @@ SET search_path TO lt_model, public;
 -- Sigef union (CAR -  SiGEF)
 DROP TABLE IF EXISTS proc1_13_car_sigef;
 CREATE TABLE proc1_13_car_sigef (
-	car INT,
-	car_geom geometry,
-	intersection_geom geometry,
-	area DOUBLE PRECISION,
-	area_original DOUBLE PRECISION,
-	sigef_area DOUBLE PRECISION,
-	shape_leng DOUBLE PRECISION,
-	area_loss NUMERIC(10,6)
+	gid INTEGER, 
+	area_loss NUMERIC(10,6), 
+	area DOUBLE PRECISION, 
+	area_original DOUBLE PRECISION, 
+	is_premium BOOLEAN, 
+	geom GEOMETRY
 );
--- Organized/proc1_14_car-sigef_2.sql
+++ Organized/proc1_14_car-sigef_2.sql
@@ -1,27 +1,30 @@
 SET search_path TO lt_model, public;
 
-INSERT INTO proc1_13_car_sigef
-SELECT *, 1.0-(ST_Area(intersection_geom)/area_original) area_loss FROM (
+INSERT INTO proc1_13_car_sigef (
+	gid,
+	area_loss,
+	area,
+	area_original,
+	is_premium,
+	geom,
+)
 SELECT 
-	a.gid car, 
-	a.geom car_geom, 
-	CASE WHEN COUNT(b.gid) = 1 THEN 
-		CASE WHEN ST_Within(a.geom, ST_GeometryN(ST_Collect(b.geom), 1)) THEN 
-			ST_GeomFromText('Polygon Empty')
-		ELSE 
-			ST_Difference(a.geom, ST_GeometryN(ST_Collect(b.geom), 1)) 
-		END 
-	WHEN COUNT(b.gid) > 1 THEN 
-		ST_Safe_Difference(a.geom, ST_Collect(b.geom)) 
-	ELSE 
-		a.geom 
-	END intersection_geom, 
+	a.gid, 
+	1.0-(ST_Area(geom)/area_original) area_loss
+	ST_Area(geom) area, 
+	area_original, 
+	is_premium,
+	geom
+	 FROM (
+SELECT 
+	a.gid, 
+	a.area_loss,
 	a.area, 
-	a.area_original,
-	MAX(ST_Area(b.geom)) sigef_area, 
-	a.perimeter shape_leng
+	a.area_original, 
+	a.is_premium, 
+	ST_SafeDifference(a.geom, ST_Collect(b.geom)) geom
 FROM lt_model.proc1_12_result a
 LEFT JOIN lt_model.lt_model_incra_pr b ON ST_Intersects(a.geom, b.geom) AND NOT ST_Touches(a.geom, b.geom) 
 	--AND ST_XMax(b.geom) < 658294.02429628 AND ST_XMin(b.geom) > 542810.3515331885 AND ST_YMax(b.geom) < -1077895.2638318148 AND ST_YMin(b.geom) > -1179502.5469183084
 WHERE (a.gid % :var_num_proc) = :var_proc
-GROUP BY a.gid, a.geom, a.area, a.perimeter, a.area_original) c;
+GROUP BY a.gid, a.area_loss, a.area, a.area_origina, a.is_premium, a.geom) c;
