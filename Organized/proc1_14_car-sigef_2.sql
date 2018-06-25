d3f626af8b25a279d8d613ef73dc8bbbe96d9953
Caio Hamamura <caiohamamura@gmail.com>
2018-06-25 14:35:23 -0300

    proc1_14 fixed missing "l"

--- Organized/proc1_14_car-sigef_2.sql
+++ Organized/proc1_14_car-sigef_2.sql
@@ -27,4 +27,4 @@ FROM lt_model.proc1_12_result a
 LEFT JOIN lt_model.lt_model_incra_pr b ON ST_Intersects(a.geom, b.geom) AND NOT ST_Touches(a.geom, b.geom) 
 	--AND ST_XMax(b.geom) < 658294.02429628 AND ST_XMin(b.geom) > 542810.3515331885 AND ST_YMax(b.geom) < -1077895.2638318148 AND ST_YMin(b.geom) > -1179502.5469183084
 WHERE (a.gid % :var_num_proc) = :var_proc
-GROUP BY a.gid, a.area_loss, a.area, a.area_origina, a.is_premium, a.geom) c;
+GROUP BY a.gid, a.area_loss, a.area, a.area_original, a.is_premium, a.geom) c;
