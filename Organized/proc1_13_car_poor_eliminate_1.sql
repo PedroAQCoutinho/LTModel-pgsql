8e61f0e14a19997fdf0a59c6caf39bce30d7e6a9
Caio Hamamura <caiohamamura@gmail.com>
2018-06-25 17:37:19 -0300

    Make result from INCRA and CAR valid

--- Organized/proc1_13_car_poor_eliminate_1.sql
+++ Organized/proc1_13_car_poor_eliminate_1.sql
@@ -122,7 +122,7 @@ FROM (
 INSERT INTO lt_model.proc1_12_car_cleaned
 SELECT gid, 1-(ST_Area(geom)/shape_area) area_loss, ST_Area(geom) area, shape_area area_original, ST_Perimeter(geom) perimeter, is_premium, geom
 FROM (SELECT gid, ST_CollectionExtract(ST_MakeValid(geom),3) geom, shape_area, area, area_original, is_premium proc1_07_car_solved
-WHERE is_premium);
+WHERE is_premium) a;
 
 UPDATE lt_model.proc1_12_car_cleaned
 SET area_original = area, area_loss = 0
