5cfa72fb7b81ebb5224c34156b6f272432e9e241
Caio Hamamura <caiohamamura@gmail.com>
2018-06-26 20:08:00 -0300

    proc1_14 return to use ST_Safe_Difference

--- Organized/proc1_14_car-sigef_2.sql
+++ Organized/proc1_14_car-sigef_2.sql
@@ -22,7 +22,7 @@ SELECT
 	a.area, 
 	a.area_original, 
 	a.is_premium, 
-	ST_Safe_Difference_Simulate(a.geom, ST_Collect(b.geom)) geom
+	ST_Safe_Difference(a.geom, ST_Collect(b.geom)) geom
 FROM lt_model.proc1_12_car_cleaned a
 LEFT JOIN lt_model.lt_model_incra_pr b ON ST_Intersects(a.geom, b.geom) AND NOT ST_Touches(a.geom, b.geom) 
 	--AND ST_XMax(b.geom) < 658294.02429628 AND ST_XMin(b.geom) > 542810.3515331885 AND ST_YMax(b.geom) < -1077895.2638318148 AND ST_YMin(b.geom) > -1179502.5469183084
