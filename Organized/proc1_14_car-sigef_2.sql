0f923f7ee117d80a2b0fa94477e83ca6b7f317a0
Caio Hamamura <caiohamamura@gmail.com>
2018-06-28 00:14:25 -0300

    proc1_14 fixed difference when only one intersection

--- Organized/proc1_14_car-sigef_2.sql
+++ Organized/proc1_14_car-sigef_2.sql
@@ -10,7 +10,11 @@ INSERT INTO proc1_13_car_sigef (
 )
 SELECT 
 	gid, 
-	1.0-(ST_Area(geom)/area_original) area_loss,
+	CASE WHEN area_original = 0 THEN 
+		0
+	ELSE
+		1.0-(ST_Area(geom)/area_original) 
+	END area_loss,
 	ST_Area(geom) area, 
 	area_original, 
 	is_premium,
@@ -22,14 +26,14 @@ SELECT
 	a.area, 
 	a.area_original, 
 	a.is_premium, 
-	CASE COUNT(b.rid)
+	ST_CollectionExtact(ST_MakeValid(CASE COUNT(b.rid)
 	WHEN 0 THEN 
 		a.geom 
 	WHEN 1 THEN
 		ST_Difference(a.geom, ST_Collect(b.geom))
 	ELSE
-		ST_Safe_Difference(a.geom, ST_Collect(b.geom))
-	END geom
+		ST_Safe_Difference(a.geom, ST_GeometryN(ST_Collect(b.geom), 1))
+	END), 3) geom
 FROM lt_model.proc1_12_car_cleaned a
 LEFT JOIN lt_model.lt_model_incra_pr b ON ST_Intersects(a.geom, b.geom) AND NOT ST_Touches(a.geom, b.geom) 
 	--AND ST_XMax(b.geom) < 658294.02429628 AND ST_XMin(b.geom) > 542810.3515331885 AND ST_YMax(b.geom) < -1077895.2638318148 AND ST_YMin(b.geom) > -1179502.5469183084
