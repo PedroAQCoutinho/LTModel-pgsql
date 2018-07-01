58de58ff100cbaa9726cc153b589262ea91e22e7
Caio Hamamura <caiohamamura@gmail.com>
2018-07-01 14:08:23 -0300

    Reorganization of scripts

--- Organized/temp.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO lt_model.result_temp 
-SELECT a.gid, ST_Intersection(a.geom, b.geom)
-FROM lt_model.result a
--- USING 
-,
-public.pa_br_limitenacional_2015_ibge_albers b 
-WHERE sub_class = 'AG' AND (a.gid % 15) = :var_proc AND NOT ST_Within(a.geom, b.geom)
