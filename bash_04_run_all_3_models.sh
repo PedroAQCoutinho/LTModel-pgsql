6c612cb87c3d902ee194a2255656fdb1e71193e1
Caio Hamamura <caiohamamura@gmail.com>
2018-07-02 18:27:57 -0300

    Fix run_all_3_models

--- bash_04_run_all_3_models.sh
+++ bash_04_run_all_3_models.sh
@@ -3,79 +3,66 @@ userName=$1
 databaseServer=$2 
 databaseName=$3
 numProc=$4
-wait=$5
-specificProc=$6
+specificProc=$5
+wait=$6
+if [ "$wait" == "" ]; then wait=false; fi
+
+function renameAppendCarTables {
+    appendSuffix=$1
+    psql -U $userName -h $databaseServer -d $databaseName -c "DROP TABLE IF EXISTS proc1_12_car_cleaned_$appendSuffix;ALTER TABLE lt_model.proc1_12_car_cleaned RENAME TO proc1_12_car_cleaned_$appendSuffix;"
+    #Create resulting lt_model_car_po and lt_model_car_pr tables
+    psql -U $userName -h $databaseServer -d $databaseName -c "DROP TABLE IF EXISTS lt_model_car_po_$appendSuffix;ALTER TABLE lt_model.lt_model_car_po RENAME TO lt_model_car_po_$appendSuffix"
+    psql -U $userName -h $databaseServer -d $databaseName -c "DROP TABLE IF EXISTS lt_model_car_pr_$appendSuffix; ALTER TABLE lt_model.lt_model_car_pr RENAME TO lt_model_car_pr_$appendSuffix"
+}
 
 
 
 #### PRIORITY RANDOM ####
 #Set priority of CAR autointersection to Random ("R")
 psql -U $userName -h $databaseServer -d $databaseName -c "UPDATE lt_model.params SET param_text = 'R' WHERE param_name = 'priority_autointersection'"
-
 #Run INCRA and CAR consistency
 ./bash_03_run_all.sh $userName $databaseServer $databaseName $numProc $wait $specificProc
-
-
-#Rename proc1_12_car_cleaned to proc1_12_car_cleaned_random
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.proc1_12_car_cleaned RENAME TO proc1_12_car_cleaned_random"
-#Create resulting lt_model_car_po and lt_model_car_pr tables
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_po RENAME TO lt_model_car_po_random"
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_pr RENAME TO lt_model_car_pr_random"
+#Rename appending random to results
+renameAppendCarTables "random"
 
 
 #### PRIORITY SMALL ####
 #Set priority of CAR autointersection to Small ("S")
 psql -U $userName -h $databaseServer -d $databaseName -c "UPDATE lt_model.params SET param_text = 'S' WHERE param_name = 'priority_autointersection'"
-./bash_03_run_all.sh $1 $2 $3 $4 $5 $6 proc1_10_car_poor_clean
-
+#Run INCRA and CAR consistency
+./bash_03_run_all.sh $userName $databaseServer $databaseName $numProc $wait proc1_10_car_poor_clean
 #Rename proc1_12_car_cleaned to proc1_12_car_cleaned_small
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.proc1_12_car_cleaned RENAME TO proc1_12_car_cleaned_small"
-#Create resulting lt_model_car_po and lt_model_car_pr tables
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_po RENAME TO lt_model_car_po_small"
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_pr RENAME TO lt_model_car_pr_small"
+renameAppendCarTables "small"
 
 
 #### PRIORITY LARGE ####
 #Set priority of CAR autointersection to Large ("L")
 psql -U $userName -h $databaseServer -d $databaseName -c "UPDATE lt_model.params SET param_text = 'L' WHERE param_name = 'priority_autointersection'"
-./bash_03_run_all.sh $1 $2 $3 $4 $5 $6 proc1_10_car_poor_clean
-
+./bash_03_run_all.sh $userName $databaseServer $databaseName $numProc $wait proc1_10_car_poor_clean
 #Rename proc1_12_car_cleaned to proc1_12_car_cleaned_large
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.proc1_12_car_cleaned RENAME TO proc1_12_car_cleaned_large"
-#Create resulting lt_model_car_po and lt_model_car_pr tables
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_po RENAME TO lt_model_car_po_large"
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_pr RENAME TO lt_model_car_pr_large"
+renameAppendCarTables "small"
 
 
 #Run overlaying rules
+function runOverlayingRules {
+    priority=$1
+    psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_po_$priority RENAME TO lt_model_car_po;"
+    psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_pr_$priority RENAME TO lt_model_car_pr;"
+    psql -U $userName -h $databaseServer -d $databaseName -f run_stmt.sql
+    psql -U $userName -h $databaseServer -d $databaseName -c "DROP TABLE IF EXISTS lt_model.result_$priority;ALTER TABLE lt_model.result RENAME TO result_$priority; ALTER INDEX lt_model.gix_result RENAME TO gix_result_$priority; CREATE TABLE lt_model.result AS SELECT * FROM lt_model.result_$priority WHERE false; CREATE INDEX gix_result ON lt_model.result USING GIST (geom);"
+    psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_po RENAME TO lt_model_car_po_$priority;"
+    psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_pr RENAME TO lt_model_car_pr_$priority;"
+}
+
 
 ## Get run statement in sql file
-psql -U $userName -h $databaseServer -d $databaseName -c "SELECT lt_model.run_statement()" > run_stmt.sql
+psql -U $userName -h $databaseServer -d $databaseName -t -c "SELECT lt_model.run_statement()" > run_stmt.sql
 
 ## Run overlaying rules for random
-priority=random
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_po_$priority RENAME TO lt_model_car_po;"
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_pr_$priority RENAME TO lt_model_car_pr;"
-psql -U $userName -h $databaseServer -d $databaseName -f run_stmt.sql
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.result RENAME TO result_$priority; ALTER INDEX lt_model.gix_result RENAME TO gix_result_$priority; CREATE TABLE lt_model.result AS SELECT * FROM lt_model.result_$priority WHERE false; CREATE INDEX gix_result ON lt_model.result USING GIST (geom);"
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_po RENAME TO lt_model_car_po_$priority;"
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_pr RENAME TO lt_model_car_pr_$priority;"
+runOverlayingRules "random"
 
 ## Run and log for small
-priority=small
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_po_$priority RENAME TO lt_model_car_po;"
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_pr_$priority RENAME TO lt_model_car_pr;"
-psql -U $userName -h $databaseServer -d $databaseName -f run_stmt.sql
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.result RENAME TO result_$priority; ALTER INDEX lt_model.gix_result RENAME TO gix_result_$priority; CREATE TABLE lt_model.result AS SELECT * FROM lt_model.result_$priority WHERE false; CREATE INDEX gix_result ON lt_model.result USING GIST (geom);"
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_po RENAME TO lt_model_car_po_$priority;"
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_pr RENAME TO lt_model_car_pr_$priority;"
-
+runOverlayingRules "small"
 
 ## Run and log for large
-priority=large
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_po_$priority RENAME TO lt_model_car_po;"
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_pr_$priority RENAME TO lt_model_car_pr;"
-psql -U $userName -h $databaseServer -d $databaseName -f run_stmt.sql
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.result RENAME TO result_$priority; ALTER INDEX lt_model.gix_result RENAME TO gix_result_$priority; CREATE TABLE lt_model.result AS SELECT * FROM lt_model.result_$priority WHERE false; CREATE INDEX gix_result ON lt_model.result USING GIST (geom);"
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_po RENAME TO lt_model_car_po_$priority;"
-psql -U $userName -h $databaseServer -d $databaseName -c "ALTER TABLE lt_model.lt_model_car_pr RENAME TO lt_model_car_pr_$priority;"
+runOverlayingRules "large"
