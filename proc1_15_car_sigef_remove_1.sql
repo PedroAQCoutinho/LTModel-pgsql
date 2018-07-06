4a6860c4ad578816181db33da314676bb43861b3
Caio Hamamura <caiohamamura@gmail.com>
2018-07-06 08:00:50 -0300

    Delete results from car before creating table, use the correct resuting table

--- proc1_15_car_sigef_remove_1.sql
+++ proc1_15_car_sigef_remove_1.sql
@@ -3,10 +3,12 @@ CREATE TABLE lt_model.proc1_14_car_sigef_cleaned AS
 SELECT *
 FROM lt_model.proc1_13_car_sigef WHERE area_loss < (SELECT param_value FROM lt_model.params WHERE param_name = 'car_incra_tolerance');
 
+DROP TABLE IF EXISTS lt_model.lt_model_car_po;
 CREATE TABLE lt_model.lt_model_car_po AS
-SELECT * FROM lt_model.proc1_13_car_sigef
+SELECT * FROM lt_model.proc1_14_car_sigef_cleaned
 WHERE NOT is_premium;
 
+DROP TABLE IF EXISTS lt_model.lt_model_car_pr;
 CREATE TABLE lt_model.lt_model_car_pr AS
-SELECT * FROM lt_model.proc1_13_car_sigef
+SELECT * FROM lt_model.proc1_14_car_sigef_cleaned
 WHERE is_premium;
