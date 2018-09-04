89b0faef3b343a3fdfd3177f17f5818073706ae9
Gerd Sparovek <atlas@icex.head.ice.cemeai.icmc.usp.br>
2018-09-04 14:03:26 -0300

    Fixed bash_02_execproc calling positional arguments

--- bash_02_execproc.sh
+++ bash_02_execproc.sh
@@ -2,6 +2,7 @@ curDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 start_date="$(date)"
 SECONDS=0
 . $curDir/bash_00_parseOptions.sh
+. $curDir/bash_01_helpers.sh
 
 procName=${@:$OPTIND:1}
 echo Running: $procName
@@ -11,12 +12,11 @@ echo
 
 SECONDS=0
 set -e
-
-psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_1.sql
+psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -f ${procName}_1.sql
 
 #If sql 2 exists execute it
-if [ -f $4_2.sql ]; then
-    for ((i=0; i < $5; i++))
+if [ -f ${procName}_2.sql ]; then
+    for ((i=0; i < ${numProc}; i++))
     do
         psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
     done
@@ -25,8 +25,8 @@ fi
 
 
 #If sql 3 exists execute it
-if [ -f $4_3.sql ]; then
-    psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_3.sql
+if [ -f ${procName}_3.sql ]; then
+    psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -f ${procName}_3.sql
 fi
 
 
