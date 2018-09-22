eea21a8157997f8fa794bf09a98ef65590b5baa3
Gerd Sparovek <atlas@icex.head.ice.cemeai.icmc.usp.br>
2018-09-22 16:12:21 -0300

    Fixed where to call get names for results

--- bash_05_run_all_overlaying.sh
+++ bash_05_run_all_overlaying.sh
@@ -10,6 +10,7 @@ function runOverlayingRules {
     psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "ALTER TABLE lt_model.lt_model_car_po_$priority RENAME TO lt_model_car_po;"
     psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "ALTER TABLE lt_model.lt_model_car_pr_$priority RENAME TO lt_model_car_pr;"
     psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -f run_stmt.sql
+    . $curDir/bash_06_add_names.sh -U $userName -h $databaseServer -d $databaseName -p $portNumber
     psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "DROP TABLE IF EXISTS lt_model.result_$priority;ALTER TABLE lt_model.result RENAME TO result_$priority; ALTER INDEX lt_model.gix_result RENAME TO gix_result_$priority; CREATE TABLE lt_model.result AS SELECT * FROM lt_model.result_$priority WHERE false; CREATE INDEX gix_result ON lt_model.result USING GIST (geom);"
     psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "ALTER TABLE lt_model.lt_model_car_po RENAME TO lt_model_car_po_$priority;"
     psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "ALTER TABLE lt_model.lt_model_car_pr RENAME TO lt_model_car_pr_$priority;"
@@ -28,4 +29,3 @@ runOverlayingRules "small"
 ## Run and log for large
 runOverlayingRules "large"
 
-. $curDir/bash_06_add_names.sh -U $userName -h $databaseServer -d $databaseName -p $portNumber
--- bash_06_add_names.sh
+++ bash_06_add_names.sh
@@ -1,7 +1,7 @@
 curDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $curDir/bash_00_parseOptions.sh
 
-psql -p $portNumber -c "\copy (SELECT table_name, column_name FROM lt_model.inputs WHERE fla_proc AND column_name IS NOT NULL) TO 'saida.csv' WITH (FORMAT 'csv')"
+psql -U $userName -h $databaseServer -d $databaseName -p $portNumber  -c "\copy (SELECT table_name, column_name FROM lt_model.inputs WHERE fla_proc AND column_name IS NOT NULL) TO 'saida.csv' WITH (FORMAT 'csv')"
 
 OLDIFS=$IFS
 IFS=,
