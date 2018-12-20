0518a9b8f378ca1ec42781defbeb7d8fe8733e5c
Felipe Cerignoni <felipe@imaflora.org>
2018-12-20 17:40:18 -0200

    change add names script

--- bash_05_run_overlaying.sh
+++ bash_05_run_overlaying.sh
@@ -9,7 +9,7 @@ function runOverlayingRules {
     psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "ALTER TABLE lt_model.lt_model_car_po_$varPriority RENAME TO lt_model_car_po;"
     psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "ALTER TABLE lt_model.lt_model_car_pr_$varPriority RENAME TO lt_model_car_pr;"
     psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -f run_stmt.sql
-    . $curDir/bash_06_add_names.sh -U $userName -h $databaseServer -d $databaseName -p $portNumber
+    . $curDir/bash_06_add_names2.sh -U $userName -h $databaseServer -d $databaseName -p $portNumber
     psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "DROP TABLE IF EXISTS lt_model.result_$varPriority;ALTER TABLE lt_model.result RENAME TO result_$varPriority; ALTER INDEX lt_model.gix_result RENAME TO gix_result_$priority; CREATE TABLE lt_model.result AS SELECT * FROM lt_model.result_$priority WHERE false; CREATE INDEX gix_result ON lt_model.result USING GIST (geom);"
     psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "ALTER TABLE lt_model.lt_model_car_po RENAME TO lt_model_car_po_$varPriority;"
     psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "ALTER TABLE lt_model.lt_model_car_pr RENAME TO lt_model_car_pr_$varPriority;"
