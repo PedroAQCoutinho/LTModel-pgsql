0fba53064a859c313293e9eb802c16e7d2e5dda8
Imageo <geoprocessamento@imaflora.org>
2019-01-14 16:24:54 -0200

    14/01/2019

--- runall.sh
+++ runall.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#PBS -N luga_ima_caio
+#PBS -N malha_db
 #PBS -l select=1:ncpus=56
 #PBS -l walltime=240:00:00
 #PBS -q atlas
 
 #Muda o diretório para o diretório do exemplo
-pg_ctl start
+pg_ctl -D ~/BDs/db  start
 # sleep 5
 cd /home/atlas/codigos/LTModel-pgsql
 
 #Não se esqueça de conferir o conteúdo do arquivo script.R
-./luga_process.sh -j 56 proc1_08_car_premium > runAll.log 2>&1
-pg_ctl stop
+./luga_process.sh -j 56 > runAll_2019.log 2>&1
+pg_ctl -D ~/BDs/db  stop
