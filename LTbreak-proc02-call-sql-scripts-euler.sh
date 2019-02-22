39433e78da476bd370708edf2111f9995cf29437
Felipe Cerignoni <felipe@imaflora.org>
2019-02-22 10:02:57 -0300

    22/02/2019

--- LTbreak-proc02-call-sql-scripts-euler.sh
+++ LTbreak-proc02-call-sql-scripts-euler.sh
@@ -6,8 +6,8 @@ LTENURE=$2
 #Altera o diretorio
 cd /home/atlas/codigos/LTModel-pgsql/
 
-psql -h localhost -U atlas -d atlas -p 5434 -v var_proc=$OMPI_COMM_WORLD_RANK -v threads=$NCPUS -v ltenure=$LTENURE -a -f LTbreak_proc01-identifying-unregistered-lands.sql &&
-psql -h localhost -U atlas -d atlas -p 5434 -v ltenure=$LTENURE -a -f LTbreak_proc02-unifying-landtenure-map.sql &&
-psql -h localhost -U atlas -d atlas -p 5434 -v var_proc=$OMPI_COMM_WORLD_RANK -v threads=$NCPUS -a -f LTbreak_proc03-breaking-by-municipality.sql &&
-psql -h localhost -U atlas -d atlas -p 5434 -v var_proc=$OMPI_COMM_WORLD_RANK -v threads=$NCPUS -a -f LTbreak_proc04-breaking-by-biome.sql &&
-psql -h localhost -U atlas -d atlas -p 5434 -v var_proc=$OMPI_COMM_WORLD_RANK -v threads=$NCPUS -a -f LTbreak_proc05-breaking-by-watershed.sql
+psql -h localhost -U atlas -d atlas -p 5434 -v var_proc=$OMPI_COMM_WORLD_RANK -v threads=$NCPUS -v ltenure=$LTENURE -f LTbreak_proc01-identifying-unregistered-lands.sql &&
+psql -h localhost -U atlas -d atlas -p 5434 -v ltenure=$LTENURE -f LTbreak_proc02-unifying-landtenure-map.sql &&
+psql -h localhost -U atlas -d atlas -p 5434 -v var_proc=$OMPI_COMM_WORLD_RANK -v threads=$NCPUS -f LTbreak_proc03-breaking-by-municipality.sql &&
+psql -h localhost -U atlas -d atlas -p 5434 -v var_proc=$OMPI_COMM_WORLD_RANK -v threads=$NCPUS -f LTbreak_proc04-breaking-by-biome.sql &&
+psql -h localhost -U atlas -d atlas -p 5434 -v var_proc=$OMPI_COMM_WORLD_RANK -v threads=$NCPUS -f LTbreak_proc05-breaking-by-watershed.sql
