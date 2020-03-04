9be7576b06691db9f9fba30dd02fc988e27f1a5b
Felipe Cerignoni <felipe@imaflora.org>
2020-03-04 10:29:14 -0300

    adição da nova regra car poor e premium

--- proc1_08_car_premium_2.sql
+++ proc1_08_car_premium_2.sql
@@ -1,32 +1,8 @@
 SET search_path TO lt_model, public;
 
-/*INSERT INTO proc1_03_is_premium 
-(
-  gid,
-  geom,
-  shape_area,
-  shape_leng,
-  area_loss,
-  new_area,
-  fla_car_premium
-)
-SELECT 
-  gid, 
-  geom,
-  shape_area,
-  shape_leng,
-  shape_area-new_area area_loss,
-  new_area,
-	CASE WHEN new_area IS NULL THEN 
-		false 
-	ELSE 
-		(new_area/shape_area) >= (SELECT param_value FROM lt_model.params WHERE param_name = 'car_premium_tolerance')
-	END fla_car_premium
-FROM (
-SELECT a.*, ST_Area(ST_CollectionExtract(b.geom,3)) new_area
-FROM proc1_00_0makevalid a
-LEFT JOIN proc1_02_car_result b ON a.gid = b.gid
-WHERE (a.gid % :var_num_proc) = :var_proc) c;*/
+\set car_table_schema `tail -1 var1.txt`
+\set car_table_name `tail -1 var2.txt`
+\set car_mf_column `tail -1 var3.txt`
 
 INSERT INTO proc1_03_is_premium 
 (
@@ -55,13 +31,13 @@ FROM (
 SELECT
     a.*,
     ST_Area(ST_CollectionExtract(b.geom,3)) new_area,
-    d.num_modulo,
+    d.:"car_mf_column" num_modulo,
     e.count_overlap
 FROM lt_model.proc1_00_0makevalid a
 LEFT JOIN lt_model.proc1_02_car_result b
     ON a.gid = b.gid
-LEFT JOIN car.input_pa_br_areaimovel_20180901_sfb d
+LEFT JOIN :"car_table_schema".:"car_table_name" d
     ON a.gid = d.gid
-LEFT JOIN (SELECT gid, COUNT(DISTINCT gid2) AS count_overlap FROM lt_model.aux_proc1_03_z1_car_intersects GROUP BY gid) e
+LEFT JOIN (SELECT gid, COUNT(DISTINCT gid2) AS count_overlap FROM lt_model.aux_proc1_03_z0_car_intersects GROUP BY gid) e
     ON a.gid = e.gid
 WHERE (a.gid % :var_num_proc) = :var_proc) c;
