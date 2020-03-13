8693ac65ad166684b39e67d688f7e7c5064dcf35
Felipe Cerignoni <felipe@imaflora.org>
2020-03-13 11:23:36 -0300

    correcao na query

--- proc1_06_car_intersects_1.sql
+++ proc1_06_car_intersects_1.sql
@@ -1,7 +1,7 @@
 SET search_path TO lt_model, public;
 
 DROP TABLE IF EXISTS proc1_03_z0_car_intersects;
-CREATE TABLE proc1_03_z1_car_intersects
+CREATE TABLE proc1_03_z0_car_intersects
 (
   gid BIGINT,
   gid2 BIGINT,
