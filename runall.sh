913f0767b7a2f83b8db248b73a78e23c79ecd417
Imageo <geoprocessamento@imaflora.org>
2020-04-22 11:19:01 -0300

    minor fixes

--- runall.sh
+++ runall.sh
@@ -2,7 +2,7 @@
 
 #PBS -N db_malha
 #PBS -l select=1:ncpus=56
-#PBS -l walltime=240:00:00
+#PBS -l walltime=240:00:00    
 #PBS -q atlas
 
 pg_ctl -D ~/BDs/db_malha  start
@@ -11,5 +11,5 @@ cd /home/atlas/codigos/LTModel-pgsql
 
 ./luga_process.sh -j 56 -p 5432 > runAll_2020.log 2>&1
 #./bash_05_run_overlaying.sh -j 56 > runAll_2020_bash_05.log 2>&1
-
+#./bash_07_add_cdmun.sh -j 56 > runAll_2020_bash_07.log 2>&1
 pg_ctl -D ~/BDs/db_malha  stop
