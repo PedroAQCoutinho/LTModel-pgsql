b96079b701be5508dd0a3cb2c44235fbf668df42
Felipe Cerignoni <felipe@imaflora.org>
2021-02-24 17:19:59 -0300

    tentativa de correcao para malha 2021

--- proc1_10_car_poor_clean_2.sql
+++ proc1_10_car_poor_clean_2.sql
@@ -3,15 +3,25 @@ SET search_path TO lt_model, public;
 INSERT INTO proc1_04_car_poor_clean
 SELECT a.gid, 
 	ST_CollectionExtract(ST_MakeValid(
+	-- CASE COUNT(b.gid) 
+	-- WHEN 0 THEN 
+	-- 	a.geom 
+	-- WHEN 1 THEN
+	-- 	CASE WHEN ST_Within(a.geom, ST_GeometryN(ST_Collect(b.geom),1)) THEN null ELSE 
+	-- 		ST_Difference(a.geom, ST_GeometryN(ST_Collect(b.geom),1))
+	-- 	END
+	-- ELSE
+	-- 		ST_Safe_Difference(a.geom, ST_Collect(b.geom))
+	-- END), 3) geom,
 	CASE COUNT(b.gid) 
 	WHEN 0 THEN 
 		a.geom 
 	WHEN 1 THEN
-		CASE WHEN ST_Within(a.geom, ST_GeometryN(ST_Collect(b.geom),1)) THEN null ELSE 
-			ST_Difference(a.geom, ST_GeometryN(ST_Collect(b.geom),1))
+		CASE WHEN ST_Within(a.geom, b.geom) THEN null ELSE 
+			ST_Difference(a.geom, b.geom))
 		END
 	ELSE
-			ST_Safe_Difference(a.geom, ST_Collect(b.geom))
+			ST_Safe_Difference(a.geom, b.geom)
 	END), 3) geom, 
 	a.shape_area
 FROM proc1_03_is_premium a
