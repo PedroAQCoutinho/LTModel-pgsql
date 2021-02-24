e1575c14fbf682fe09848903269c51fdad36062d
Felipe Cerignoni <felipe@imaflora.org>
2021-02-24 17:49:53 -0300

    tentativa de correcao para malha 2021

--- proc1_10_car_poor_clean_2.sql
+++ proc1_10_car_poor_clean_2.sql
@@ -3,26 +3,16 @@ SET search_path TO lt_model, public;
 INSERT INTO proc1_04_car_poor_clean
 SELECT a.gid, 
 	ST_CollectionExtract(ST_MakeValid(
-	-- CASE COUNT(b.gid) 
-	-- WHEN 0 THEN 
-	-- 	a.geom 
-	-- WHEN 1 THEN
-	-- 	CASE WHEN ST_Within(a.geom, ST_GeometryN(ST_Collect(b.geom),1)) THEN null ELSE 
-	-- 		ST_Difference(a.geom, ST_GeometryN(ST_Collect(b.geom),1))
-	-- 	END
-	-- ELSE
-	-- 		ST_Safe_Difference(a.geom, ST_Collect(b.geom))
-	-- END), 3) geom,
 	CASE COUNT(b.gid) 
 	WHEN 0 THEN 
 		a.geom 
 	WHEN 1 THEN
-		CASE WHEN ST_Within(a.geom, b.geom) THEN null ELSE 
-			ST_Difference(a.geom, b.geom)
+		CASE WHEN ST_Within(a.geom, ST_GeometryN(ST_Collect(b.geom),1)) THEN null ELSE 
+			ST_Difference(a.geom, ST_GeometryN(ST_Collect(b.geom),1))
 		END
 	ELSE
-			ST_Safe_Difference(a.geom, b.geom)
-	END), 3) geom, 
+			ST_Safe_Difference(a.geom, ST_GeometryN(ST_Collect(b.geom),1))
+	END), 3) geom,
 	a.shape_area
 FROM proc1_03_is_premium a
 LEFT JOIN proc1_03_z1_car_intersects c ON a.gid = c.gid
