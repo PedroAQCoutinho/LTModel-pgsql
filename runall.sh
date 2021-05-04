6a1a4352fc7a5faf774974d7fc0a01e09e98d22e
Geoprocessamento <geoprocessamento@imaflora.org>
2021-05-04 15:59:42 -0300

    malha_2021

--- runall.sh
+++ runall.sh
@@ -9,7 +9,7 @@ pg_ctl -D ~/BDs/db_malha  start
 
 cd /home/atlas/codigos/LTModel-pgsql
 
-#./luga_process.sh -j 56 -p 5432 > runAll_2020_break.log 2>&1
+#./luga_process.sh -j 56 -p 5432 > runAll_2021.log 2>&1
 ./bash_05_run_overlaying.sh -j 56 > runAll_2020_break_bash_05.log 2>&1
 
 pg_ctl -D ~/BDs/db_malha  stop
