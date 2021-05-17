9db9c8a23ebdf234f03028ea113e5c4d5d3de3db
Geoprocessamento <geoprocessamento@imaflora.org>
2021-05-17 10:22:07 -0300

    new example

--- runall.sh
+++ runall.sh
@@ -10,6 +10,8 @@ pg_ctl -D ~/BDs/db_malha  start
 cd /home/atlas/codigos/LTModel-pgsql
 
 #./luga_process.sh -j 56 -p 5432 > runAll_2021.log 2>&1
+#./luga_process.sh -j 40 -p 5434 proc1_07_car_autointersection > runAll_2021_from_proc1_07.log 2>&1
+
 ./bash_05_run_overlaying.sh -j 56 > runAll_2020_break_bash_05.log 2>&1
 
 pg_ctl -D ~/BDs/db_malha  stop
