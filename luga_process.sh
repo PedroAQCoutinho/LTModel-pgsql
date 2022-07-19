764a1bf495bcc1731225dd63e2769a4748262ab6
PedroAQCoutinho <paq.coutinho@gmail.com>
2022-07-19 16:53:58 -0300

    Ajusting lt_model. to recorte.

--- luga_process.sh
+++ luga_process.sh
@@ -6,17 +6,17 @@ curDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function renameAppendCarTables {
     appendSuffix=$1
-    psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "DROP TABLE IF EXISTS lt_model.proc1_12_car_cleaned_$appendSuffix;ALTER TABLE lt_model.proc1_12_car_cleaned RENAME TO proc1_12_car_cleaned_$appendSuffix;ALTER INDEX lt_model.gix_proc1_12_car_cleaned RENAME TO gix_proc1_12_car_cleaned_$appendSuffix;"
+    psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "DROP TABLE IF EXISTS recorte.proc1_12_car_cleaned_$appendSuffix;ALTER TABLE recorte.proc1_12_car_cleaned RENAME TO proc1_12_car_cleaned_$appendSuffix;ALTER INDEX recorte.gix_proc1_12_car_cleaned RENAME TO gix_proc1_12_car_cleaned_$appendSuffix;"
     #Create resulting lt_model_car_po and lt_model_car_pr tables
-    psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "DROP TABLE IF EXISTS lt_model.lt_model_car_po_$appendSuffix;ALTER TABLE lt_model.lt_model_car_po RENAME TO lt_model_car_po_$appendSuffix; CREATE INDEX gix_lt_model_car_po_$appendSuffix ON lt_model.lt_model_car_po_$appendSuffix USING GIST (geom);"
-    psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "DROP TABLE IF EXISTS lt_model.lt_model_car_pr_$appendSuffix; ALTER TABLE lt_model.lt_model_car_pr RENAME TO lt_model_car_pr_$appendSuffix; CREATE INDEX gix_lt_model_car_pr_$appendSuffix ON lt_model.lt_model_car_pr_$appendSuffix USING GIST (geom);"
+    psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "DROP TABLE IF EXISTS recorte.lt_model_car_po_$appendSuffix;ALTER TABLE recorte.lt_model_car_po RENAME TO lt_model_car_po_$appendSuffix; CREATE INDEX gix_lt_model_car_po_$appendSuffix ON recorte.lt_model_car_po_$appendSuffix USING GIST (geom);"
+    psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "DROP TABLE IF EXISTS recorte.lt_model_car_pr_$appendSuffix; ALTER TABLE recorte.lt_model_car_pr RENAME TO lt_model_car_pr_$appendSuffix; CREATE INDEX gix_lt_model_car_pr_$appendSuffix ON recorte.lt_model_car_pr_$appendSuffix USING GIST (geom);"
 }
 
 
 
 #### PRIORITY RANDOM ####
 #Set priority of CAR autointersection to Random ("R")
-psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "UPDATE lt_model.params SET param_text = 'R' WHERE param_name = 'priority_autointersection'"
+psql -U $userName -h $databaseServer -d $databaseName -p $portNumber -c "UPDATE recorte.params SET param_text = 'R' WHERE param_name = 'priority_autointersection'"
 #Run INCRA and CAR consistency
 ./bash_03_run_all.sh -U $userName -h $databaseServer -d $databaseName -p $portNumber -j $numProc $useWait $specificProc
 #Rename appending random to results
