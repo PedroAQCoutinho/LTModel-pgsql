7fa1ad72a3ea8e3f2a63c6d65eab6a8929ed1382
PedroAQCoutinho <paq.coutinho@gmail.com>
2022-07-20 15:23:13 -0300

    Change schema of the function simplify_multipolygon_name

--- proc2_get_name_result.sql
+++ proc2_get_name_result.sql
@@ -4,5 +4,5 @@ ADD COLUMN IF NOT EXISTS name TEXT;
 UPDATE recorte.result a
 SET name = b.:var_column
 FROM recorte.:var_table b
-WHERE a.table_source = recorte.simplify_multipolygon_name(LEFT(:'var_table', 63)) AND a.original_gid = b.gid;
+WHERE a.table_source = lt_model.simplify_multipolygon_name(LEFT(:'var_table', 63)) AND a.original_gid = b.gid;
 
