8ee5f533bc4ebc85be14161717ac8a90ef5a8c32
PedroAQCoutinho <paq.coutinho@gmail.com>
2022-08-10 11:59:35 -0300

    REGEX REPAIR

--- lt_model_public_functions.sql
+++ lt_model_public_functions.sql
@@ -543,7 +543,7 @@ CREATE OR REPLACE FUNCTION lt_model.simplify_multipolygon_name(var_table_name te
  LANGUAGE sql
  IMMUTABLE
 AS $function$
-	SELECT LEFT(regexp_replace(var_table_name, '([^]_)$', '_sim\1'), 63);
+	SELECT LEFT(regexp_replace(var_table_name, '(_[^_]*_*)$', '_sim\1'), 63);
 $function$
 ;
 
