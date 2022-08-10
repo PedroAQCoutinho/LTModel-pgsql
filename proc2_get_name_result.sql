8b599c8114064dec2d5e6f362369d0c7d0e5c06b
PedroAQCoutinho <paq.coutinho@gmail.com>
2022-08-10 16:28:00 -0300

    Little ajust on set <a>.name = ...

--- proc2_get_name_result.sql
+++ proc2_get_name_result.sql
@@ -2,7 +2,7 @@ ALTER TABLE lt_model.result
 ADD COLUMN IF NOT EXISTS name TEXT;
 
 UPDATE lt_model.result a
-SET name = b.:var_column
+SET a.name = b.:var_column
 FROM lt_model.:var_table b
 WHERE a.table_source = lt_model.simplify_multipolygon_name(LEFT(:'var_table', 63)) AND a.original_gid = b.gid;
 
